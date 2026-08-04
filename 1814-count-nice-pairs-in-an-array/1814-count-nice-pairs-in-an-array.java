7849c0588d1ef1ab3937a8e7bb2843d4607df41c
Ved varshney <161019815+VedVarshney@users.noreply.github.com>
2026-08-04 22:48:21 +0530

    Time: 42 ms (31.97%), Space: 68.6 MB (85.86%) - LeetHub - Time: O(N), Space: O(N)

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.java
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.java
@@ -1,42 +1,23 @@
-// class Solution {
-//     public int rev(int n){
-//         int rv=0;
-//         for(int i=n; i>0; i/=10)
-//         rv=rv*10+i%10;
-//         return rv;
-//     }
-//     public int countNicePairs(int[] arr) {
-//     int n=arr.length;
-//     int c=0;
-//     for(int i=0; i<n; i++){
-//         for(int j=i+1; j<n; j++){
-//         if(arr[i]+rev(arr[j])==(arr[j])+rev(arr[i]))
-//         c++;
-//         }
-//     }   
-//     return c;
-//     }
-// }
 class Solution {
     public int rev(int n){
         int rv=0;
         for(int i=n; i>0; i/=10)
         rv=rv*10+i%10;
         return rv;
-    } 
+    }
     public int countNicePairs(int[] arr) {
     HashMap<Integer,Integer> map = new HashMap<>();
-    int c=0; 
-    for(int i=0; i<arr.length; i++){
-    int ans=arr[i]-rev(arr[i]);
-    if(map.containsKey(ans)){
-       c+=map.get(ans);
-       c%=1000000007;
-       map.put(ans,map.get(ans)+1);
-    }else{
-        map.put(ans,1);
-    }
-    } 
+    int c=0;
+    for(int ele : arr){
+        int diff=ele-rev(ele);
+        if(map.containsKey(diff)){
+           c+=map.get(diff);
+           c%=1000000007;
+           map.put(diff,map.get(diff)+1);
+        }else{
+           map.put(diff,1);
+        }
+    }   
     return c;
     }
 }
