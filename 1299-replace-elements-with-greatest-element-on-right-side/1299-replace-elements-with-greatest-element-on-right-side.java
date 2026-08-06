6b6f5e2d8692f47a13b02cb21c8a682d66a719aa
Ved varshney <161019815+VedVarshney@users.noreply.github.com>
2026-08-06 14:51:47 +0530

    Time: 3 ms (69.56%), Space: 49.5 MB (12.01%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+    int n=arr.length;
+    int[] ans = new int[n];
+    ans[n-1]=-1;
+    int nge=arr[n-1];
+    for(int i=n-2; i>=0; i--){
+        ans[i]=nge;
+        nge=Math.max(nge,arr[i]);
+    }  
+    return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
