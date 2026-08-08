57c2fbca4cf9e1cdcc769918df33c98f7e8f092f
Ved varshney <161019815+VedVarshney@users.noreply.github.com>
2026-08-08 12:59:44 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 701213-rotate-array-GFG/701213-rotate-array.java
@@ -0,0 +1,22 @@
+class Solution {
+     public void rev(int[] arr , int i , int j){
+        while(i<j){
+            int t =arr[i];
+            arr[i]=arr[j];
+            arr[j]=t;
+            i++;
+            j--;
+        }
+    }
+    public void rotateArr(int arr[], int k) {
+     int n=arr.length;
+    k=k%n;
+    rev(arr,0,k-1);
+    rev(arr,k,n-1);
+    rev(arr,0,n-1);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
