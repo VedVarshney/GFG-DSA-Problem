0e7732384fd0f69dec3f8af8405256cd631668e5
Ved varshney <161019815+VedVarshney@users.noreply.github.com>
2026-08-09 22:39:58 +0530

    Time: 19 ms (57.71%), Space: 46.4 MB (88.13%) - LeetHub - Time: O(N), Space: O(N)

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.java
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.java
@@ -2,35 +2,34 @@ class Solution {
     public int[] findEvenNumbers(int[] arr) {
     HashMap<Integer,Integer> map = new HashMap<>();
     ArrayList<Integer> al = new ArrayList<>();
-    for(int ele : arr){
-        if(map.containsKey(ele)){
-            map.put(ele,map.get(ele)+1);
-        }else{
-            map.put(ele,1);
-        }
-    }  
+    for(int ele:arr){
+        if(map.containsKey(ele))
+        map.put(ele,map.get(ele)+1);
+        else
+        map.put(ele,1);
+    }
     for(int i=100; i<=998; i+=2){
     int x=i;
     int c=x%10; x/=10;
     int b=x%10; x/=10;
-    int a=x%10; 
+    int a=x;
     if(map.containsKey(a)){
-       int aFrq=map.get(a);
-       map.put(a,aFrq-1);
-       if(aFrq==1) map.remove(a);
-    if(map.containsKey(b)){
-       int bFrq=map.get(b);
-       map.put(b,bFrq-1);
-       if(bFrq==1) map.remove(b);
-    if(map.containsKey(c)){
+        int afrq=map.get(a);
+        map.put(a,afrq-1);
+        if(afrq==1) map.remove(a);
+        if(map.containsKey(b)){
+        int bfrq=map.get(b);
+        map.put(b,bfrq-1);
+        if(bfrq==1) map.remove(b);
+        if(map.containsKey(c)){
         al.add(i);
+        }
+        map.put(b,bfrq); 
+        }
+        map.put(a,afrq);
     }
-    map.put(b,bFrq);   
-    }
-    map.put(a,aFrq);
-    }
-    }
-    int[] ans=new int[al.size()];
+    }   
+    int[] ans = new int[al.size()];
     for(int i=0; i<al.size(); i++){
         ans[i]=al.get(i);
     }
