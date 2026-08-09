19d8d09dbf822194a9c3dd3db4beacc49b51cea4
Ved varshney <161019815+VedVarshney@users.noreply.github.com>
2026-08-09 12:43:38 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 704025-sum-of-natural-number-cubes-GFG/704025-sum-of-natural-number-cubes.java
@@ -0,0 +1,9 @@
+class Solution {
+    int sumOfSeries(int n) {
+    return n*n*(n+1)*(n+1)/4;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
