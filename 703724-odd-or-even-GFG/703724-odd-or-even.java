9de3241cd8d80b1d32da670af2e8cf5669fde1e7
Ved varshney <161019815+VedVarshney@users.noreply.github.com>
2026-08-09 12:39:31 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 703724-odd-or-even-GFG/703724-odd-or-even.java
@@ -0,0 +1,11 @@
+class Solution {
+    static boolean isEven(int n) {
+    if(n%2==0)
+    return true;
+    return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
