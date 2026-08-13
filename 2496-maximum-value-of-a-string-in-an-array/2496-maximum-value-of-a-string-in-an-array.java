8d684651191ccfe8ccdb04561b97f2cc7d4ab618
Ved varshney <161019815+VedVarshney@users.noreply.github.com>
2026-08-13 23:25:42 +0530

    Time: 5 ms (16.76%), Space: 43.1 MB (28.7%) - LeetHub - Time: O(n*m), Space: O(n)

---
+++ 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maximumValue(String[] arr) {
+    int n=arr.length;
+    int[] ans = new int[n];    
+    for(int i=0; i<arr.length; i++){
+    String s=arr[i];
+    boolean flag=true;
+    String sum="";
+    for(int j=0; j<s.length(); j++){
+        if(s.charAt(j)>=97 && s.charAt(j)<=122){
+           flag=false;
+           break;
+        }else{
+            sum+=s.charAt(j);
+        }
+    }
+    if(!flag) 
+    ans[i]=s.length();
+    else
+    ans[i]=Integer.parseInt(sum);
+    }
+    int mx=Integer.MIN_VALUE;
+    for(int i=0; i<ans.length; i++){
+    if(ans[i]>mx)
+    mx=ans[i];    
+    }
+    return mx;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
