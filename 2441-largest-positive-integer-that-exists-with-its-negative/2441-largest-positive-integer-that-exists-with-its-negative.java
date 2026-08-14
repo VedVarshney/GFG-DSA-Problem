3a2e5a20246e8cd3525ccb6655210e6ef012bf1b
Ved varshney <161019815+VedVarshney@users.noreply.github.com>
2026-08-14 16:07:49 +0530

    Time: 3 ms (96.96%), Space: 47.3 MB (28.8%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMaxK(int[] arr) {
+    HashSet<Integer> set = new HashSet<>();
+    for(int ele : arr){
+        if(ele<0)
+        set.add(ele);
+    }
+    int mx=Integer.MIN_VALUE;
+    for(int i=0; i<arr.length; i++){
+        if(arr[i]>0 && arr[i]>mx){
+        if(set.contains(-arr[i]))
+        mx=arr[i];
+        }
+    } 
+    if(mx==Integer.MIN_VALUE) return -1;
+    return mx; 
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
