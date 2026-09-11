8515a6b4d624f3fc4e0294dc6c97d0c47bec2619
Ved varshney <161019815+VedVarshney@users.noreply.github.com>
2026-09-11 16:55:17 +0530

    Time: 0 ms (100%), Space: 44.5 MB (8.29%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] arr, int x) {
+    int c=0;
+    for(int i=0; i<arr.length; i++){
+        if(arr[i]>=x) c++;
+    }    
+    return c;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
