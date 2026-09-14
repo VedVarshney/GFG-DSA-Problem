c9ce4fe0daf0a73e894e12a24b73330c45c0a5cb
Ved varshney <161019815+VedVarshney@users.noreply.github.com>
2026-09-14 23:03:03 +0530

    Time: 799 ms (7.27%), Space: 79.9 MB (13.5%) - LeetHub - Time: O(n log log n), Space: O(n)

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,24 +1,28 @@
 class Solution {
     public int countPrimes(int n) {
-    if(n<=2) return 0;
-    boolean[] arr = new boolean[n];  
-    for(int i=2; i<n; i++){
-        arr[i]=true;
-    } 
-    for(int i=2; i*i<n; i++){
-        if(arr[i]) {
-        for(int j=i*i; j<n; j+=i)
-        arr[j]=false;
+        if (n <= 2) {
+            return 0;
         }
-    }
-    int c=0;
-    for(int i=2; i<n; i++){
-        if(arr[i]) c++;
-    }
-    return c;
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+        return count;
     }
 }
-
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
